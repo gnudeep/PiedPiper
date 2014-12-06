ae24df099e81adbdd97b7141fb04da70c499213a
deependra@gmail.com <deependra@gmail.com>
2014-12-06 05:49:16 +0000

    proxy updates

--- CustomServices/Heartbeat/src/main/java/com/piedpiper/Heartbeat.java
+++ CustomServices/Heartbeat/src/main/java/com/piedpiper/Heartbeat.java
@@ -1,16 +1,35 @@
 package com.piedpiper;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+
 import javax.ws.rs.*;
+import javax.ws.rs.core.StreamingOutput;
 
 @Path("/")
 public class Heartbeat {
 	
 	@GET
+	@HEAD
     @Path("/check")
-    @Produces("application/json")
-    public String checkHearbeat(){
+    @Produces("text/plain")
+    public StreamingOutput checkHearbeat(){
         
-        return "Lub Dub";
+        return new StreamingOutput() {
+            @Override
+            public void write(OutputStream outputStream) throws IOException, WebApplicationException {
+            	 Writer writer = new BufferedWriter(new OutputStreamWriter(outputStream));
+
+                 for (int i = 0; i < 2000;i++) {
+                     writer.write("<m0:symbol>IBM</m0:symbol>" + "\n");
+                 }
+                 writer.flush();
+                //outputStream.write("Deependra".getBytes());
+            }
+        };
     }
 	
 }
